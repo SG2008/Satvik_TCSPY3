01402111ed48bb8af2c4c7bddb21789c2b92065b
Satvik Goel <satvikgoel@yahoo.com>
2020-06-22 17:23:29 -0500

    code for chapter 14 exercises

--- chap14ex.py
+++ chap14ex.py
@@ -10,38 +10,127 @@ def test(did_pass):
     print(msg)
 
 
-xs = [1,3,5,7,9,11,13,15,17,19]
-ys = [4,8,12,16,20,24]
+xs = [1,2,5,5,8]
+ys = [2,8,11]
 zs = xs+ys
 zs.sort()
 
+
 def test_suite():
-    test(merge(xs, []) == xs)
-    test(merge([], ys) == ys)
+    test(mergeA(xs, ys) == [2, 8])
+    test(mergeA([1,3,4,5,5], [1,2,4,5,6]) == [1, 4, 5])
+    test(mergeB(xs, ys) == [1, 5, 5])
+    test(mergeB([1,3,4,5,5], [1,2,4,5,6]) == [3, 5])
+    test(mergeC(xs, ys) == [11])
+    test(mergeC([1,3,4,5,5], [1,2,4,5,6]) == [2, 6])
+    test(mergeD(xs, ys) == [1, 5, 5, 11])
+    test(mergeD([1,3,4,5,5], [1,2,4,5,6]) == [2, 3, 5, 6])
 
 
-def merge(xs, ys):
-
+def mergeA(xs, ys):
+    """ merge sorted lists xs and ys. Return a sorted result """
     result = []
     xi = 0
     yi = 0
 
     while True:
-        if xi >= len(xs):
-            result.extend(ys[yi:])
+        if xi >= len(xs):          # If xs list is finished,
+            return result          # And we're done.
+
+        if yi >= len(ys):          # Same again, but swap roles
             return result
 
-        if yi >= len(ys):
+        # Both lists still have items, copy smaller item to result.
+        if xs[xi] == ys[yi]:
+            result.append(xs[xi])
+            xi += 1
+            yi += 1
+        elif xs[xi] < ys[yi]:
+            xi += 1
+        else:
+            yi += 1
+        print(result)
+
+
+def mergeB(xs, ys):
+    """ merge sorted lists xs and ys. Return a sorted result """
+    result = []
+    xi = 0
+    yi = 0
+
+    while True:
+        if xi >= len(xs):          # If xs list is finished,
+            return result          # And we're done.
+
+        if yi >= len(ys):     # Same again, but swap roles
             result.extend(xs[xi:])
             return result
 
+        # Both lists still have items, copy smaller item to result.
+        if xs[xi] == ys[yi]:
+            xi += 1
+            yi += 1
+        elif xs[xi] < ys[yi]:
+            result.append(xs[xi])
+            xi += 1
+        else:
+            yi += 1
+        print(result)
 
-        if xs[xi] <= ys[yi]:
+
+def mergeC(xs, ys):
+    """ merge sorted lists xs and ys. Return a sorted result """
+    result = []
+    xi = 0
+    yi = 0
+
+    while True:
+        if xi >= len(xs):          # If xs list is finished,
+            result.extend(ys[yi:])
+            return result          # And we're done.
+
+        if yi >= len(ys):          # Same again, but swap roles
+            return result
+
+        # Both lists still have items, copy smaller item to result.
+        if xs[xi] == ys[yi]:
+            xi += 1
+            yi += 1
+        elif xs[xi] < ys[yi]:
+            xi += 1
+        else:
+            result.append(ys[yi])
+            yi += 1
+        print(result)
+
+
+def mergeD(xs, ys):
+    """ merge sorted lists xs and ys. Return a sorted result """
+    result = []
+    xi = 0
+    yi = 0
+
+    while True:
+        if xi >= len(xs):          # If xs list is finished,
+            result.extend(ys[yi:])
+            return result          # And we're done.
+
+        if yi >= len(ys):          # Same again, but swap roles
+            result.extend(xs[xi:])
+            return result
+
+        # Both lists still have items, copy smaller item to result.
+        if xs[xi] == ys[yi]:
+            xi += 1
+            yi += 1
+        elif xs[xi] < ys[yi]:
             result.append(xs[xi])
             xi += 1
         else:
             result.append(ys[yi])
             yi += 1
+        print(result)
 
 
 test_suite()
+
